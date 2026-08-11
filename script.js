678cd8e992e9a6b7bf7d8782fffce2f5f49b4095
gapurvagrace-max <gapurvagrace@gmail.com>
2026-08-11 12:41:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const heroContent = document.querySelector(".hero-content");
+const aboutSection = document.querySelector(".about");
 
 window.addEventListener("load", () => {
     heroContent.classList.add("show");
+});
+
+window.addEventListener("scroll", () => {
+
+    const sectionPosition = aboutSection.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight * 0.8;
+
+    if (sectionPosition < screenPosition) {
+        aboutSection.classList.add("show");
+    }
+
 });
